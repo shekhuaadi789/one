21466add3f030dd3867cbde18cf02504592e2806
shekhuaadi789 <143437593+shekhuaadi789@users.noreply.github.com>
2024-09-15 16:08:28 +0530

    Add files via upload

---
+++ install.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+from fan import installer
+
+if __name__ == '__main__':
+	installer.cli()
---
+++ run.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+from fan import core
+
+if __name__ == '__main__':
+    core.cli()
